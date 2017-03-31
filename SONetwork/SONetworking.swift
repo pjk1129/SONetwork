e22db81344e3efdd884c1d060fcb619d768fee33
Jecky <pjk1129@users.noreply.github.com>
2017-03-31 11:44:55 +0800

    Update SONetworking.swift
    更改为open

--- SONetwork/SONetworking.swift
+++ SONetwork/SONetworking.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 
-class SONetworking: NSObject {
+open class SONetworking: NSObject {
     
-    class func request( _ method : String = "GET", url : String , parameters : Dictionary<String,AnyObject> = [:], success : @escaping (_ result : String?)->Void, fail:@escaping (_ error : NSError?)->Void){
+    open class func request( _ method : String = "GET", url : String , parameters : Dictionary<String,AnyObject> = [:], success : @escaping (_ result : String?)->Void, fail:@escaping (_ error : NSError?)->Void){
         
         SOSessionManager.default.request(method, url: url, parameters: parameters, success: { (data) in
             //此处只返回最原始的数据
